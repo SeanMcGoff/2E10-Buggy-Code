8a4be3b52c2a363221baea086576fb27862f5d8c
Sean McGoff <srobertmcgoff@gmail.com>
2024-03-14 13:16:33 +0000

    Fixed Speed Setting

--- buggy-code.ino
+++ buggy-code.ino
@@ -11,7 +11,8 @@ bool obstacle_detected = false;             // Obstacle Detected Flag
 unsigned long US_Trigger_Timer = millis();  // Loop For US Sensor Processing
 unsigned long Web_Timer = millis();         // Timer for Web Processing
 
-int speed = 255;
+int speed = 255;         // Speed (as defined by PID)
+double speed_coeff = 1;  // Speed coefficient (defined by GUI)
 
 
 // Updates the Buggy's Motors Given IR State
@@ -19,19 +20,19 @@ void updateBuggyMotors() {
   IR::updateState();
   if (IR::state[0] == HIGH && IR::state[1] == HIGH) {
     // On the white path
-    Motors::leftForward(speed);
-    Motors::rightForward(ceil(speed * Motors::CORRECTION));
+    Motors::leftForward(ceil(speed * speed_coeff));
+    Motors::rightForward(ceil(speed * Motors::CORRECTION * speed_coeff));
   } else if (IR::state[0] == LOW && IR::state[1] == LOW) {
     // Not on any path
     Motors::bothStop();
   } else if (IR::state[0] == LOW && IR::state[1] == HIGH) {
     // Turn Left
-    Motors::rightForward(ceil(speed * Motors::CORRECTION));
+    Motors::rightForward(ceil(speed * Motors::CORRECTION * speed_coeff));
     Motors::leftForward(0);
   } else {
     // Turn Right
-    Motors::leftForward(speed);
-    Motors::rightBackward(25);
+    Motors::leftForward(ceil(speed * speed_coeff));
+    Motors::rightBackward(ceil(speed * speed_coeff * 0.1));
   }
 }
 
@@ -65,7 +66,7 @@ void parseGUIRequest(JsonDocument doc, WiFiClient client) {
     double clientSpeed = doc["speed"];
     Serial.print("Setting speed to: ");
     Serial.println(clientSpeed);
-    speed = clientSpeed;
+    speed_coeff = clientSpeed;
   }
 
   // Incorrect / Empty Command
@@ -109,6 +110,7 @@ void setup() {
 
 void loop() {
 
+  // Check for Client Request 500 Milliseconds
   if (millis() - Web_Timer >= 500) {
     WiFiClient client = Net::server.available();  // listen for incoming clients
     if (client && client.available()) {
@@ -118,18 +120,21 @@ void loop() {
     Web_Timer = millis();
   }
 
-  WheelEncoders::update();  // Update the Wheel Encoders
-
-  obstacle_detected = US::getCurrentDistance() <= US::DISTANCE_SENS;
-
+  // Trigger US Sensor Every 250 Milliseconds
   if (millis() - US_Trigger_Timer >= 250) {
     US::Trigger();
     US_Trigger_Timer = millis();
   }
 
-  speed = PID::computePID(US::getCurrentDistance());
+  obstacle_detected = US::getCurrentDistance() <= US::DISTANCE_SENS;  // Update Obstacle Detection
 
+  WheelEncoders::update();  // Update the Wheel Encoders
+
+  speed = PID::computePID(US::getCurrentDistance());  // Update Speed with PID
+
+  // Update Buggy Motors
   if (Motors::activated) updateBuggyMotors();
+  // If motors are deactivated, stop motors and reset distance traveled
   else {
     Motors::bothStop();
     WheelEncoders::distanceTraveled = 0;
--- pid.h
+++ pid.h
@@ -15,7 +15,7 @@ const double SET_POINT = 15;  // Target Distance (in cm) away from object
 extern unsigned long currentTime, previousTime;       // Time Variables
 extern double error, lastError, cumError, rateError;  // Error Variables
 
-int computePID(double distance);
+int computePID(double distance);  // computes new PID and updates relevant variables
 
 }
 
